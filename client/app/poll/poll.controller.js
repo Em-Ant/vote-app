5732c62b5bed2f0304cc84c771869f969ad693f7
EmAnt <joshua999x@gmail.com>
2015-09-18 01:21:57 +0200

    Improve poll page

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -1,17 +1,36 @@
 'use strict';
 
 angular.module('fullstackApp')
-  .controller('PollCtrl',['$scope', '$routeParams', '$http', 'Auth', '$location', function ($scope, $routeParams, $http, Auth, $location) {
+  .controller('PollCtrl',
+    ['$scope', '$routeParams', '$http', 'Auth', '$location',
+      function ($scope, $routeParams, $http, Auth, $location) {
+
+    $scope.nextView = "Results"
+    $scope.voteMessage = Auth.isLoggedIn() ? "Click on the Question to Vote"
+      : "You must be Logged In to Vote";
+
     $http.get('/api/polls/' + $routeParams.id).success(function(res){
+
       $scope.poll = res;
-     });   
-    
+      $scope.poll.showResult = false;
+
+     });
+
+    $scope.toggleView = function() {
+
+      $scope.poll.showResult = !$scope.poll.showResult;
+      if($scope.poll.showResult) $scope.nextView = "Questions";
+      else  $scope.nextView = "Results";
+    };
+
     $scope.voteFor = function(id) {
+
+      console.log(id);
       if(Auth.isLoggedIn()) {
-      
+
       } else {
         $location.path('/login');
       }
     };
-   
+
   }]);
