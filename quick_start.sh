9191959b1778b3e5a1d99440711a069c658969ad
EmAnt <joshua999x@gmail.com>
2015-09-18 18:08:54 +0200

    add quickstart script

---
+++ quick_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+#  QUICK APP START SCRIPT
+#
+
+console="xfce4-terminal";
+
+# Works only if in a terminal
+tty -s;
+if [ $? -eq 0 ];
+  then
+
+  # Start DB in another terminal
+  $console -e "./mongod";
+
+  # Source nvm
+  . ~/.nvm/nvm.sh;
+
+  # Load NodeJS and Start Express Server
+  nvm use 0 && grunt serve;
+fi
